4230197bdaed0814fa7cb21bc00f1a695a427a39
dkhayutin <dan.khayutin@gmail.com>
2018-05-09 03:17:25 +0000

    character position exercise

---
+++ characterPosition.js
@@ -0,0 +1,17 @@
+var phrase = process.argv.slice(2).join(' ')
+
+
+function countLetters(sentence) {
+  var letterCounter = {}
+  for(var i = 0; i < sentence.length; i++) {
+    if (sentence.charAt(i) !== ' ') {
+      if (letterCounter[sentence.charAt(i)]) {
+        letterCounter[sentence.charAt(i)] = letterCounter[sentence.charAt(i)] + 1
+      } else {
+        letterCounter[sentence.charAt(i)] = 1
+       }
+     }
+   }
+  return letterCounter
+}
+console.log(countLetters(phrase))
--- count-letters.js
+++ count-letters.js
@@ -7,9 +7,13 @@ function countLetters(sentence) {
   for(var i = 0; i < sentence.length; i++) {
     if (sentence.charAt(i) !== ' ') {
       if (letterCounter[sentence.charAt(i)]) {
-        letterCounter[sentence.charAt(i)] = letterCounter[sentence.charAt(i)] + 1
+        letterCounter[sentence.charAt(i)].push(i)
+        //key, then push the new position
+        //old: letterCounter[sentence.charAt(i)] = letterCounter[sentence.charAt(i)] + 1
       } else {
-        letterCounter[sentence.charAt(i)] = 1
+        letterCounter[sentence.charAt(i)] = [i]
+        //create key = [position for the letter]
+        //old: letterCounter[sentence.charAt(i)] = 1
        }
      }
    }
